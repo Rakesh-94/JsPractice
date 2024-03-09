8760bcd131d6d5bb85d7d4f9a6c81cbb00baa939
apple <apple@Rakesh-Pal-React-native.local>
2024-03-09 09:27:45 +0530

    max/min 9Mar 2024

--- simpleJsQuestion.js
+++ simpleJsQuestion.js
@@ -1,8 +1,26 @@
 //Q: find duplicate
-const array = [1, 2, 3, 4, 5, 6, 2, 4];
 
-const duplicate = array.filter((item, index, arr) => {
-  return index !== arr.indexOf(item);
-});
+// const array = [1, 2, 3, 4, 5, 6, 2, 4];
 
-console.log(duplicate);
+// const duplicate = array.filter((item, index, arr) => {
+//   return index !== arr.indexOf(item);
+// });
+
+// console.log(duplicate);
+
+// Q how to find max and min value in a given array
+
+const array = [1, 2, 3, 4, 10, 30, 22, 50, 12, 0, 89];
+
+//Q first methods
+// const max = Math.max(...array);
+// const min = Math.min(...array);
+// console.log(max, min);
+
+// function maxMin(arrays) {
+//   return console.log(Math.max(...arrays), Math.min(...arrays));
+// }
+// maxMin(array);
+
+// const max = array.reduce((prev, currt) => (prev < currt ? currt : prev));
+// console.log(max);
