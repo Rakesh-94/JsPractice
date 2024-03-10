fcacc8d3b7b8a115f563e9f216c7951ded807558
apple <apple@Rakesh-Pal-React-native.local>
2024-03-10 09:28:06 +0530

    diffrence between == and === 10Mar 2024

--- simpleJsQuestion.js
+++ simpleJsQuestion.js
@@ -10,7 +10,7 @@
 
 // Q how to find max and min value in a given array
 
-const array = [1, 2, 3, 4, 10, 30, 22, 50, 12, 0, 89];
+// const array = [1, 2, 3, 4, 10, 30, 22, 50, 12, 0, 89];
 
 //Q first methods
 // const max = Math.max(...array);
@@ -24,3 +24,10 @@ const array = [1, 2, 3, 4, 10, 30, 22, 50, 12, 0, 89];
 
 // const max = array.reduce((prev, currt) => (prev < currt ? currt : prev));
 // console.log(max);
+
+//Q waht is the difference between "==" and "===" imn javascript
+// Both are comparison operator
+// The difference between both the operators is that "==" is used to compare values whereas "===" is used to compare both value and types
+// x = 5;
+// y = "5";
+// console.log(x == y);
