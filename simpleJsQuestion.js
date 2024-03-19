af4076782ce61112988d71bf4cb245fd8edec87f
apple <apple@Rakesh-Pal-React-native.local>
2024-03-19 23:56:25 +0530

    19March 2024

--- simpleJsQuestion.js
+++ simpleJsQuestion.js
@@ -159,24 +159,39 @@
 //   }
 // }
 
-let number = 2;
-let isPrime = true;
+// let number = 2;
+// let isPrime = true;
 
-if (number == 1) {
-  console.log(`${number} is not a prime neither a composite`);
-} else if (number < 1) {
-  console.log(`Prime number for ${number} is not possible`);
-} else {
-  for (let i = 2; i < number; i++) {
-    if (number % i == 0) {
-      isPrime = false;
-      break;
-    }
-  }
+// if (number == 1) {
+//   console.log(`${number} is not a prime neither a composite`);
+// } else if (number < 1) {
+//   console.log(`Prime number for ${number} is not possible`);
+// } else {
+//   for (let i = 2; i < number; i++) {
+//     if (number % i == 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+//   if (isPrime) {
+//     console.log(`${number} is a prime number`);
+//   } else {
+//     console.log(`${number} is not a prime number`);
+//   }
+// }
 
-  if (isPrime) {
-    console.log(`${number} is a prime number`);
-  } else {
-    console.log(`${number} is not a prime number`);
-  }
-}
+//Q how to find vowels form sting i js
+// first method
+// const vowel = "e";
+
+// if (
+//   vowel == "a" ||
+//   vowel == "e" ||
+//   vowel == "i" ||
+//   vowel == "o" ||
+//   vowel == "u"
+// ) {
+//   console.log("this is vovel");
+// } else {
+//   console.log("this is not vovel");
+// }
