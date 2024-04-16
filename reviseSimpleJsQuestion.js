58ac4729543ef16e0f59a5d13aee961c322547ca
rakesh_pal <rakesh.pal@mobulous.com>
2024-04-16 22:19:12 +0530

    16Apr 2024

---
+++ reviseSimpleJsQuestion.js
@@ -0,0 +1,12 @@
+// Q duplicate find
+
+// const arr = [1, 2, 34, 4, 5, 6, 6, 7, 9, 2, 4];
+
+// const duplicat = arr.filter((item, ind, arr) => {
+//   console.log(ind);
+//   console.log("br");
+//   console.log(arr.indexOf(item));
+//   return ind !== arr.indexOf(item);
+// });
+
+// console.log(duplicat);
