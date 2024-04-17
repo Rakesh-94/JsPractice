336c2ffc96ca1b9ca2d7e9eabad656da4e635d84
rakesh_pal <rakesh.pal@mobulous.com>
2024-04-18 00:00:37 +0530

    17March 2024

--- reviseSimpleJsQuestion.js
+++ reviseSimpleJsQuestion.js
@@ -10,3 +10,23 @@
 // });
 
 // console.log(duplicat);
+
+// indexOf method
+// const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+// console.log(beasts.indexOf('bison'));
+// Expected output: 1
+
+// Start from index 2
+// console.log(beasts.indexOf('bison', 2));
+// Expected output: 4
+
+// console.log(beasts.indexOf('giraffe'));
+// Expected output: -1
+
+// const array = [2, 9, 9];
+// array.indexOf(2); // 0
+// array.indexOf(7); // -1
+// array.indexOf(9, 2); // 2
+// array.indexOf(2, -1); // -1
+// array.indexOf(2, -3); // 0
