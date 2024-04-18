5af585de6f27ef88b11249763577d029e56adb10
rakesh_pal <rakesh.pal@mobulous.com>
2024-04-18 19:07:45 +0530

    18Apr 2024

--- reviseSimpleJsQuestion.js
+++ reviseSimpleJsQuestion.js
@@ -30,3 +30,17 @@
 // array.indexOf(9, 2); // 2
 // array.indexOf(2, -1); // -1
 // array.indexOf(2, -3); // 0
+
+// Q reduce method
+
+// const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
+// Expected output: 10
