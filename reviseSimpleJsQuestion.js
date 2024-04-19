a40f6ae767979e2d1cacf4099e7adadb7d2d085c
rakesh_pal <rakesh.pal@mobulous.com>
2024-04-19 09:26:21 +0530

    19Apr 2024

--- reviseSimpleJsQuestion.js
+++ reviseSimpleJsQuestion.js
@@ -44,3 +44,12 @@
 
 // console.log(sumWithInitial);
 // Expected output: 10
+
+// Q map() method
+// const array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+// const map1 = array1.map((x) => x * 2);
+
+// console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
