90fce934d5394d3d03d2c489670f9283efc14292
rakesh_pal <rakesh.pal@mobulous.com>
2024-04-28 17:20:32 +0530

    (commit one day later) 27Apr 2024

---
+++ The Complete JavaScript Course/CodingChallenge.js
@@ -0,0 +1,47 @@
+// JavaScript Fundamentals – Part 1
+
+// Coding Challenge #1
+
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+// Your tasks:
+
+// 1. StoreMark'sandJohn'smassandheightinvariables
+// 2. CalculateboththeirBMIsusingtheformula(youcanevenimplementboth
+// versions)
+// 3. CreateaBooleanvariable'markHigherBMI'containinginformationabout
+// whether Mark has a higher BMI than John.
+
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+// GOOD LUCK 😀
+
+// ***** solving start *****
+
+// const markWeight = 78;
+// const markHeight = 1.69;
+
+// const jonhWeight = 92;
+// const jonhHeight = 1.95;
+
+// const markWeight = 95;
+// const markHeight = 1.88;
+
+// const jonhWeight = 85;
+// const jonhHeight = 1.76;
+
+// const BMIsMark = markWeight / markHeight ** 2;
+// const BMIsJonh = jonhWeight / jonhHeight ** 2;
+// console.log(BMIsMark, BMIsJonh);
+
+// const markHigherBMI = BMIsMark > BMIsJonh;
+
+// markHigherBMI
+//   ? console.log(`Mark bmi ${BMIsMark} is grather than Jonh ${BMIsJonh}`)
+//   : console.log(`Jonh bmi ${BMIsJonh} is grather than mark bmi ${BMIsMark}`);
+
+// console.log(typeof markHigherBMI);
+// console.log(typeof "first");
+
+// ***** solving End *****
