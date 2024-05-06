13ce980d25d6d41b83c2633d68a029ed7b9d3386
rakesh_pal <rakesh.pal@mobulous.com>
2024-05-06 17:09:24 +0530

    one day later

--- TheCompleteJavaScriptCourse/assignments.js
+++ TheCompleteJavaScriptCourse/assignments.js
@@ -286,4 +286,25 @@
 // arabic: '5th most spoken language'
 // for all other simply log 'Great language too :D'
 
+// const language = "mandarin";
+
+// switch (language) {
+//   case "chinese":
+//   case "mandarin":
+//     console.log("MOST number of native speakers!");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("Number 4'");
+//     break;
+//   case "arabic":
+//     console.log("5th most spoken language");
+//   default:
+//     console.log(`Great language too :${language}`);
+// }
 // ***** solving End *****
